0b2d9a19e475b698f854850b33e66b67d08d9969
sarazeynalzada <szeynalzada18182@ada.edu.az>
2025-12-16 19:54:43 +0400

    database configuration added

--- app/config.py
+++ app/config.py
@@ -7,6 +7,15 @@ class Config:
     # SECURITY: Set SECRET_KEY in env for production.
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key-change-me")
 
+    # DB
+    INSTANCE_DIR = BASE_DIR / "instance"
+    INSTANCE_DIR.mkdir(exist_ok=True)
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        f"sqlite:///{(INSTANCE_DIR / 'adabuy.sqlite').as_posix()}",
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
     # Uploads
     UPLOAD_FOLDER = (BASE_DIR / "app" / "static" / "uploads").as_posix()
     MAX_CONTENT_LENGTH = 2 * 1024 * 1024  # 2MB
